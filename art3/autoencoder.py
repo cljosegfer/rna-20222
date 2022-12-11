22dd9aebd2978464c52b6da939ab1b1ca2e3e63d
jose geraldo fernandes <macaverde604@gmail.com>
2022-12-11 18:57:43 -0300

    corrige metodo ae

---
+++ art3/autoencoder.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 11 16:16:07 2022
+
+@author: jose
+"""
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from embedding import autoencoder
+from dataset import custom
+from hparams import epochs, batch_size, datasets, K
+
+from tqdm import tqdm
+
+from sklearn.metrics import roc_auc_score
+
+log = []
+for dataset in datasets:
+    auc = 0
+    for fold_n in tqdm(range(K)):
+        # read
+        train = custom(dataset, fold_n)
+        val = custom(dataset, fold_n, train = False)
+        train_loader = torch.utils.data.DataLoader(train, 
+                                                   batch_size = batch_size, 
+                                                   shuffle = True)
+        
+        # model
+        model = autoencoder(in_features = train.in_features())
+        optimizer = optim.Adam(model.parameters(), weight_decay = 1e-4)
+        criterion = nn.MSELoss()
+        
+        # train
+        for epoch in range(epochs):
+            for batch, y in train_loader:
+                batch = batch.float()
+                
+                optimizer.zero_grad()
+                xhat = model.forward(batch)
+                loss = criterion(xhat, batch)
+                loss.backward()
+                optimizer.step()
+        
+        # logistic
+        model.fit(train.X, train.y)
+        
+        # eval
+        yhat = model.predict(torch.Tensor(val.X))
+        y = val.y
+        auc += roc_auc_score(y, yhat) / K
+    log.append(auc)
--- art3/embedding.py
+++ art3/embedding.py
@@ -6,12 +6,15 @@ Created on Sun Dec 11 10:32:13 2022
 @author: jose
 """
 
+import torch
 import torch.nn as nn
+import numpy as np
 
 class autoencoder(nn.Module):
-    def __init__(self, p = 100, **kwargs):
+    def __init__(self, p = 100, l = 1e-3, **kwargs):
         super().__init__()
         self.p = p
+        self.l = l
         
         self.encoder = nn.Sequential(
             nn.Linear(kwargs['in_features'], self.p), 
@@ -19,6 +22,8 @@ class autoencoder(nn.Module):
         
         self.decoder = nn.Sequential(
             nn.Linear(self.p, kwargs['in_features']))
+        
+        self.W = None
     
     def forward(self, x):
         latent = self.encoder(x)
@@ -27,6 +32,24 @@ class autoencoder(nn.Module):
     
     def latent(self, x):
         return self.encoder(x)
+    
+    def fit(self, x, y):
+        H = self.encoder(torch.Tensor(x)).detach().numpy()
+        H = self._concatenate(H)
+        
+        A = np.linalg.pinv(H.T @ H + self.l * np.identity(self.p + 1))
+        self.W = A @ H.T @ y
+    
+    def _concatenate(self, X):
+        return np.hstack((np.ones(shape = (X.shape[0], 1)), X))
+    
+    def _sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+    
+    def predict(self, x):
+        H = self.encoder(x).detach().numpy()
+        yhat = self._sigmoid(self._concatenate(H) @ self.W)
+        return yhat
 
 class mlp(nn.Module):
     def __init__(self, p = 100, **kwargs):
